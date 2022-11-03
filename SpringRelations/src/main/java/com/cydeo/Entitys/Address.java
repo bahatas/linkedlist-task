a4e2b4d940b19833336eaae718d682a0a5a7ce4c
Kramer Brown <brownkramer63@gmail.com>
2022-11-03 15:07:27 -0500

    add adress and client relationship

--- SpringRelations/src/main/java/com/cydeo/Entitys/Address.java
+++ SpringRelations/src/main/java/com/cydeo/Entitys/Address.java
@@ -4,25 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
-@Table(name="clients")
+@Table(name = "address")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Client extends BaseEntity {
 
-    String clientName;
-    String email;
-    String phone;
-@ManyToOne
-    Address address;
-
-
+public class Address extends BaseEntity {
+    String description;
+    String zipCode;
+    String state;//todo add relationship
 }
